a605214f32cfc257e57ee3828cf2e0802db16e78
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-21 14:31:07 +0200

    change form action link in index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
     <div>
         <p>A felhasználó feladatainak megtekintéséhez, jelentkezz be a levélben megadott email cím és jelszó segítségével:</p>
-        <form method="post" action="index.php">
+        <form method="post" action="app/connect_and_make_request.php">
             <b>E-mail</b>: <input type="text" name="email"><br><br>
             <b>Jelszó</b>: <input type="password" name="password"><br><br>
             <input type="submit" value="Bejelentkezés">
