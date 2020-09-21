649e95885ffc203843916979feb2a907ec5e15f9
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-21 12:22:16 +0200

    create index.php (page for logging in)

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p>A felhasználó feladatainak megtekintéséhez, jelentkezz be a levélben megadott email cím és jelszó segítségével:</p>
+        <form method="post" action="index.php">
+            <b>E-mail</b>: <input type="text" name="email"><br><br>
+            <b>Jelszó</b>: <input type="password" name="password"><br><br>
+            <input type="submit" value="Bejelentkezés">
+        </form>
+    </div>
+    
+</body>
+</html>
