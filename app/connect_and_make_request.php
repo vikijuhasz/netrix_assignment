9879cbb9a72c50f5a515e3234d37c5d90bfaa79b
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-21 14:27:26 +0200

    app/connect_and_make_request.php

---
+++ app/connect_and_make_request.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once('../vendor/autoload.php');
+
+use App\Classes\User;
+use App\Classes\Request;
+
+// !!! validation 
+$email = $_POST['email'];
+$password = $_POST['password'];
+$user = new User($email, $password);
+$token = $user->connectToACAccount();
+
+$request = new Request($token);
+
+
