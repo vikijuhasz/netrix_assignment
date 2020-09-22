e9c01a1912477b4b08f0d6adac5fa9b84a0e3732
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-22 13:37:28 +0200

    modify text of instruction and button on index page

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
             } 
             unset($_SESSION['error']);
         ?>
-        <p>A felhasználó feladatainak megtekintéséhez, jelentkezz be a levélben megadott email cím és jelszó segítségével:</p>
+        <p>A felhasználó feladatainak megtekintéséhez, add meg a levélben megadott email címet és jelszót:</p>
         <form method="post" action="app/connect_and_make_request.php">
             <b>E-mail</b>: <input type="text" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>"><br><br>
             <b>Jelszó</b>: <input type="password" name="password"><br><br>
-            <input type="submit" value="Bejelentkezés">
+            <input type="submit" value="Feladatok megtekintése">
         </form>
     </div>
     
