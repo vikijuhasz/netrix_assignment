a70cbadc142d1ae5b97f0db80df82e861c81b461
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-22 10:20:33 +0200

    create tasks.php page

---
+++ tasks.php
@@ -0,0 +1,43 @@
+<?php 
+    session_start(); 
+    $tasks = $_SESSION['tasks'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p><h3>A felhasználó 20 legutóbb módosított feladata</h3></p>
+        <table>
+            <tr>
+                <th>Feladat neve</th>
+                <th>Feladat leírása</th>
+                <th>Feladat utolsó módosításának dátuma</th>
+            </tr>
+            <?php foreach ($tasks as $updated_on => $task) :
+                $date = new DateTime("@$updated_on");
+                $timezone = new DateTimeZone('Europe/Budapest'); 
+                $date->setTimezone($timezone);                  
+                $updated_on = $date->format('Y-m-d H:i:s');
+            ?>
+                <tr>
+                    <td><?=$task['name']?></td>
+                    <td><?=$task['body']?></td>
+                    <td><?=$updated_on?></td>
+                </tr>
+            <?php endforeach; ?>            
+        </table>
+    </div>
+    
+</body>
+</html>        
+
+
+
+
