77d22b11996b18ef3868f6cef54490ebae84a2ef
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-22 10:18:17 +0200

    add noOFTasks variable to helper function

--- app/classes/Helper.php
+++ app/classes/Helper.php
@@ -4,7 +4,7 @@ namespace App\Classes;
 
 class Helper
 {
-    public static function makeTasksArrayOfAssignee($tasks, $assignee_id)
+    public static function makeTasksArrayForDisplayOfAssignee($tasks, $assignee_id, $noOfTasks)
     { 
         $tasksArr = [];
         foreach ($tasks['tasks'] as $task) { 
@@ -14,6 +14,7 @@ class Helper
         } 
         
         krsort($tasksArr);
-        return $tasksArr;
+        $tasksArrSlice = array_slice($tasksArr, 0, $noOfTasks, true);
+        return $tasksArrSlice;
     }
 }
