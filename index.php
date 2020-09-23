8c4f2c1ea8c6852ce3dce71ce4a09db2e4dde42d
vikijuhasz <viktoriajuhasz8@gmail.com>
2020-09-23 08:52:27 +0200

    modify display of error message

--- index.php
+++ index.php
@@ -4,23 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Bejelentkezés</title>
 </head>
 <body>
     <div>
-        <?php 
-            if (isset($_SESSION['error'])) {
-                echo ($_SESSION['error']); 
-            } 
-            unset($_SESSION['error']);
-        ?>
         <p>A felhasználó feladatainak megtekintéséhez, add meg a levélben megadott email címet és jelszót:</p>
         <form method="post" action="app/controller.php">
             <b>E-mail</b>: <input type="text" name="email"><br><br>
             <b>Jelszó</b>: <input type="password" name="password"><br><br>
             <input type="submit" value="Feladatok megtekintése">
         </form>
+        <p class="error-message">
+        <?php 
+            if (isset($_SESSION['error'])) {
+                echo ($_SESSION['error']); 
+            } 
+            unset($_SESSION['error']);
+        ?>
+        </p>
     </div>
     
 </body>
